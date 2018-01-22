fa5ec6ad14399d387efa93f9eb8c4ee0e93d14d3
austin-2017 <sharon.austin.2017@gmail.com>
2018-01-21 20:09:44 -0500

    added login and modified main

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:ok@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'y337kGcys&zP3B'
 
 
 class Blog(db.Model):
