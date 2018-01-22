d12a1da231e54293c4559aeb009768349bb23bb6
austin-2017 <sharon.austin.2017@gmail.com>
2018-01-21 22:49:21 -0500

    significant portion removed from blog.html see email

--- main.py
+++ main.py
@@ -10,11 +10,23 @@ app.secret_key = 'y337kGcys&zP3B'
 
 from models import db, User, Blog
 
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    if request.method == 'POST':
+        blog_title = request.form['title']
+        new_blog = Blog(blog_title)
+        db.session.add(new_blog)
+        db.session.commit()
+
+    blogs = Blog.query.all()
+    return render_template('blog.html',title="Blog!", blogs=blogs)
+
 @app.before_request
 def require_login():
-    allowed_routes = ['login', 'signup']
+    allowed_routes = ['index', 'login', 'signup']
     if request.endpoint not in allowed_routes and 'email' not in session:
-        return redirect('/login')
+        return redirect('/')
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -34,16 +46,16 @@ def login():
 
 # TODO start of validation pw needs to be aligned with words password and verification
 
-@app.route("/", methods=['POST'])
+@app.route("/signup", methods=['POST'])
 def validate_fields():
     username = request.form["username"]
-    pw1 = request.form["pw1"]
-    pw2 = request.form["pw2"]
+    password = request.form["password"]
+    verify = request.form["verify"]
     email = request.form["email"]
 
     username_error = ''
-    pw1_error = ''
-    pw2_error = ''
+    password_error = ''
+    verify_error = ''
     pw_error = ''
     email_error = ''
 
@@ -57,25 +69,25 @@ def validate_fields():
         username = username
     #https://www.infoworld.com/article/2655121/security/password-size-does-matter.html
     #https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/password
-    if len(pw1) < 3:
-        pw1 = pw1
-        pw1_error = 'Password must contain more than 3characters long, 20 max, minimum 14 recommended'
+    if len(password) < 3:
+        password = password
+        password_error = 'Password must contain more than 3characters long, 20 max, minimum 14 recommended'
 
-    if len(pw2) < 3:
-        pw2 = pw2
-        pw2_error = 'Verification password must contain more than 3 characters long, 20 max, minimum 14 recommended' 
+    if len(verify) < 3:
+        verify = verify
+        verify_error = 'Verification password must contain more than 3 characters long, 20 max, minimum 14 recommended' 
 
-    if len(pw1) > 20:
-        pw1 = pw1
-        pw1_error = 'Password is too long, 20 max, minimum 14 recommended'
+    if len(password) > 20:
+        password = password
+        password_error = 'Password is too long, 20 max, minimum 14 recommended'
 
-    if len(pw2) > 20:
-        pw2 = pw2
-        pw2_error = 'Verification password is too long, 20 max, minimum 14 recommended'    
+    if len(verify) > 20:
+        verify = verify
+        verify_error = 'Verification password is too long, 20 max, minimum 14 recommended'    
 
-    if pw1 != pw2:
-        pw1 = pw1
-        pw2 = pw2
+    if password != verify:
+        password = password
+        verify = verify
         pw_error = 'Passwords do not match'
 
     #Criteria for email are that it has a single @, a single ., contains no spaces, and is between 3 and 20 characters long
@@ -90,18 +102,18 @@ def validate_fields():
 
     if username == "":
         username_error = 'Username must be more than 3 characters but no more than 20'
-    if pw1 == "":
-        pw1_error = 'Set a password, no fewer than 3 and no longer than 20 characters'
-    if pw2 == "":
-        pw2_error = 'Enter a password to match the one above, no fewer than 3 and no longer than 20 characters'
+    if password == "":
+        password_error = 'Set a password, no fewer than 3 and no longer than 20 characters'
+    if verify == "":
+        verify_error = 'Enter a password to match the one above, no fewer than 3 and no longer than 20 characters'
 
 
-    if not username_error and not pw_error and not pw1_error and not pw2_error and not email_error:
+    if not username_error and not password_error and not verify_error and not pw_error and not email_error:
         return render_template('hello_greeting.html', username = username)
 
     else:
-        return render_template('hello_form.html', pw_error=pw_error, username_error=username_error, pw1_error=pw1_error, pw2_error=pw2_error, email_error=email_error,
-        username=username, pw1=pw1, pw2=pw2, email=email)
+        return render_template('hello_form.html', pw_error=pw_error, username_error=username_error, password_error=password_error, verify_error=verify_error, email_error=email_error,
+        username=username, password=password, pw2=pw2, email=email)
 # TODO end of validation pw needs to be aligned with words password and verification 
 @app.route('/logout')
 def logout():
@@ -177,19 +189,6 @@ def new_post():
     return render_template('/newpost.html')
 
 @app.route('/newpost', methods=['POST', 'GET'])        
-
-
-@app.route('/', methods=['POST', 'GET'])
-def index():
-
-    if request.method == 'POST':
-        blog_title = request.form['title']
-        new_blog = Blog(blog_title)
-        db.session.add(new_blog)
-        db.session.commit()
-
-    blogs = Blog.query.all()
-    return render_template('blog.html',title="Blog!", blogs=blogs)
 
 
 @app.route('/delete-blog', methods=['POST'])
